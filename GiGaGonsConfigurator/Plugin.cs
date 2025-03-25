dbebe33a9753c25da9fdd242676bb5ed5add1dc0
MeGaGiGaGon <107241144+MeGaGiGaGon@users.noreply.github.com>
2025-03-24 23:41:33 -0700

    IL finally works time to redo it all

--- GiGaGonsConfigurator/Plugin.cs
+++ GiGaGonsConfigurator/Plugin.cs
@@ -19,6 +19,15 @@ namespace GiGaGonsConfigurator
     {
         internal static new ManualLogSource Logger;
         private static readonly Harmony Patcher = new(PluginInfo.PLUGIN_GUID);
+        public class ModConfig
+        {
+            public static ConfigEntry<int> levelLengthMul;
+
+            public static void InitConfig(ConfigFile config)
+            {
+                levelLengthMul = config.Bind("Configuration", "Level Length Multiplier", 1, "WARNING: The game stops working above 2\nAlso doubles level generation time");
+            }
+        }
         private void Awake()
         {
             Logger = base.Logger;
@@ -27,65 +36,34 @@ namespace GiGaGonsConfigurator
         }
         private void OnDestroy()
         {
-            if (Chainloader.ManagerObject.hideFlags != HideFlags.HideAndDontSave) return;
+            if (Chainloader.ManagerObject != null && Chainloader.ManagerObject.hideFlags != HideFlags.HideAndDontSave) return;
             Patcher.UnpatchSelf();
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} was unloaded!");
         }
     }
-
-    //[HarmonyPatch(typeof(LevelMapGenerator), "ConfigSpecialNodes")]
-    //[HarmonyDebug]
-    //public static class Patch_LevelMapGeneration_ConfigSpecialNodes
-    //{
-    //    [HarmonyDebug]
-    //    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-    //    {
-    //        System.Console.Write("Test2");
-    //        var matcher = new CodeMatcher(instructions);
-    //        matcher.MatchForward(false,
-    //            new CodeMatch(OpCodes.Ldc_I4_5)
-    //        );
-    //        matcher.SetOpcodeAndAdvance(OpCodes.Ldc_I4_1);
-    //        matcher.SearchForward(x => x.opcode == OpCodes.Ldc_R4 && (float)x.operand == 0.3f);
-    //        matcher.SetOperandAndAdvance(1.0f);
-    //        return matcher.InstructionEnumeration();
-    //    }
-    //}
-
 }
 
-//[HarmonyPatch(typeof(LevelAssembler), "GetLevel")]
-//public static class Patch_LevelAssembler_GetLevel
-//{
-//    [HarmonyDebug]
-//    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-//    {
-//        var matcher = new CodeMatcher(instructions);
-//        matcher.MatchForward(true,
-//            new CodeMatch(OpCodes.Ldloc_1),
-//            new CodeMatch(OpCodes.Ldarg_3)
-//        );
-//        matcher.InsertAndAdvance(
-//            new CodeInstruction(OpCodes.Ldc_I4_5),
-//            new CodeInstruction(OpCodes.Mul)
-//        );
-//        matcher.MatchForward(true,
-//            new CodeMatch(OpCodes.Ldloc_1),
-//            new CodeMatch(OpCodes.Ldarg_3)
-//        );
-//        matcher.InsertAndAdvance(
-//            new CodeInstruction(OpCodes.Ldc_I4_5),
-//            new CodeInstruction(OpCodes.Mul)
-//        );
-//        return matcher.InstructionEnumeration();
-//    }
-//}
-
-[HarmonyPatch(typeof(LevelAssembler), nameof(LevelAssembler.GetLevel))]
-internal static class Patch_LevelAssembler_GetLevel
+[HarmonyPatch(typeof(LevelAssembler), "GetLevel")]
+public static class Patch_LevelAssembler_GetLevel
 {
-    internal static bool Prefix(int difficulty, GameObject biome, int length = 6, float slope = 0f)
+    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
     {
-        return false;
+        var matcher = new CodeMatcher(instructions);
+        CodeMatch[] opcodes_to_match = {
+            new CodeMatch(OpCodes.Ldloc_1),
+            new CodeMatch(OpCodes.Ldarg_3)
+        };
+        matcher.MatchForward(false, opcodes_to_match);
+        matcher.Advance(opcodes_to_match.Length);
+        matcher.InsertAndAdvance(
+            CodeInstruction.Call(() => 1 )
+        );
+        matcher.MatchForward(false, opcodes_to_match);
+        matcher.Advance(opcodes_to_match.Length);
+        matcher.InsertAndAdvance(
+            new CodeInstruction(OpCodes.Ldc_I4_5),
+            new CodeInstruction(OpCodes.Mul)
+        );
+        return matcher.InstructionEnumeration();
     }
 }
